d59446dae7197a209714ec2c32c308342cb2cebb
Nate Gomes <nategomes@Nates-MBP.attlocal.net>
2024-02-12 20:10:20 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+app.get('/',(req,res) =>
+{
+    res.send('Welcome to an Awesome app about Breads')
+});
+
+
+app.use('/breads', require('./controllers/breads_controller'))
+
+app.listen(PORT,() => {
+    console.log(`listening on PORT${PORT}`)
+})
+
